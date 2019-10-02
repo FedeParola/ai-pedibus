424d930a3da8de648eb2bf45d43c6cfba374e44e
FedeParola <fede.parola@hotmail.it>
2019-10-02 15:27:32 +0200

    Initial project setup

---
+++ pedibus-backend/src/main/java/it/polito/ai/pedibusbackend/PedibusBackendApplication.java
@@ -0,0 +1,13 @@
+package it.polito.ai.pedibusbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PedibusBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PedibusBackendApplication.class, args);
+    }
+
+}
---
+++ pedibus-frontend/src/app/app.component.spec.ts
@@ -0,0 +1,35 @@
+import { TestBed, async } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule
+      ],
+      declarations: [
+        AppComponent
+      ],
+    }).compileComponents();
+  }));
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have as title 'pedibus-frontend'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app.title).toEqual('pedibus-frontend');
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('.content span').textContent).toContain('pedibus-frontend app is running!');
+  });
+});
