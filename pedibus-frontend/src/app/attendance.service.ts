a56f9b47ee39fe9ddbd4a74345f248d13d99410f
FedeParola <fede.parola@hotmail.it>
2019-10-16 09:52:19 +0200

    Attendance view partial implementation

--- pedibus-frontend/src/app/attendance.service.ts
+++ pedibus-frontend/src/app/attendance.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { forkJoin } from 'rxjs/index';
+import { map } from 'rxjs/operators';
+
 import { environment } from '../environments/environment'
 
 @Injectable({
@@ -13,24 +16,75 @@ export class AttendanceService {
     return this.http.get(environment.apiUrl+'/lines');
   }
 
-  getLine(lineId: number) {
+  getStops(lineId: number) {
     return this.http.get(environment.apiUrl+'/lines/'+lineId);
   }
 
-  getAvailabilities(username: string, lineId: number) {
-    return this.http.get(environment.apiUrl+'/users/'+username+'/availabilities?consolidatedOnly=true&lineId='+lineId);
+  getPupils(lineId: number) {
+    return this.http.get(environment.apiUrl+'/lines/'+lineId+'/pupils');
   }
 
-  getReservations(lineName: string, date: Date) {
-    return this.http.get(environment.apiUrl+'/reservations/'+lineName+"/"+date.toISOString().substring(0, 10));
+  getRides(username: string, lineId: number) {
+    return this.http.get(environment.apiUrl+'/users/'+username+'/rides?lineId='+lineId);
   }
 
-  createAttendance(lineName: string, date: Date, pupilId: number, direction: string) {
-    return this.http.post(environment.apiUrl+'/attendances/'+lineName+"/"+date.toISOString().substring(0, 10), {pupilId, direction});
+  /**
+   * Get attendances and reservations for the given ride and returns a data structure that combines
+   * the information
+   * @param rideId
+   */
+  getRideData(rideId: number) {
+    let attendances$ = this.http.get(environment.apiUrl+'/rides/'+rideId+'/attendances');
+    let reservations$ = this.http.get(environment.apiUrl+'/rides/'+rideId+'/reservations');
+
+    return forkJoin([attendances$, reservations$]).pipe(
+      map(([attendances, reservations]: [any[], any[]]) => {
+        let dataMap = new Map();
+
+        /* Scan all attendances and add them */
+        for (let attendance of attendances) {
+          if (!dataMap.has(attendance.stopId)) {
+            dataMap.set(attendance.stopId, []);
+          }
+
+          let data = {
+            type: attendance.hasReservation ? 'both' : 'attendance',
+            attendanceId: attendance.id,
+            pupil: attendance.pupil,
+            stopId: attendance.stopId
+          }
+
+          dataMap.get(attendance.stopId).push(data);
+        }
+
+        /* Scan all reservations and add only the ones without attendance */
+        for (let reservation of reservations) {
+          if (!reservation.hasAttendance) {
+            if (!dataMap.has(reservation.stopId)) {
+              dataMap.set(reservation.stopId, []);
+            }
+
+            let data = {
+              type: 'reservation',
+              pupil: reservation.pupil,
+              stopId: reservation.stopId
+            }
+
+            dataMap.get(reservation.stopId).push(data);
+          }
+        }
+
+        return dataMap;
+      })
+    )
   }
 
-  deleteAttendance(lineName: string, date: Date, attendanceId: number) {
-    return this.http.delete(environment.apiUrl+'/attendances/'+lineName+"/"+date.toISOString().substring(0, 10)+"/"+attendanceId.toString());
+  createAttendance(pupilId: number, rideId: number, stopId) {
+    return this.http.post(environment.apiUrl+'/attendances', {pupilId, rideId, stopId});
+  }
+
+  deleteAttendance(attendanceId: number) {
+    return this.http.delete(environment.apiUrl+'/attendances/'+attendanceId);
   }
 
 }
--- pedibus-frontend/src/app/attendance/attendance.component.ts
+++ pedibus-frontend/src/app/attendance/attendance.component.ts
@@ -13,15 +13,14 @@ import { MatSnackBar } from '@angular/material';
   styleUrls: ['./attendance.component.css']
 })
 export class AttendanceComponent implements OnInit {
-  currentDate;
-  currentLine;
-  reservations$: Observable<any>;
   lines;
-  lineData$;
-  availabilities;
-  currentAvailability;
-  nextEnabled;
-  prevEnabled;
+  selectedLine;
+  stops;
+  pupils;
+  rides;
+  selectedRideIndex = -1;
+  rideData;
+  missingPupils;
 
   constructor(private attendanceService: AttendanceService,
               private authenticationService: AuthenticationService,
@@ -29,154 +28,215 @@ export class AttendanceComponent implements OnInit {
               private _snackBar: MatSnackBar) {}
 
   ngOnInit() {
-    this.attendanceService.getLines().subscribe((res) => {
-      this.lines = res;
-      this.currentLine = this.lines[0];
-      this.lineData$ = this.attendanceService.getLine(this.currentLine.id);
-      this.loadAvailabilities();
-      
-    },
-    (error) => {
-      this.handleError(error)
-    });
+    this.attendanceService.getLines().subscribe(
+      (res) => {
+        this.lines = res;
+        this.selectedLine = this.lines[0];
+        this.loadLine();
+        
+      },
+      (error) => {
+        this.handleError(error)
+      }
+    );
   }
 
-  loadAvailabilities() {
-    this.availabilities = null;
-    this.attendanceService.getAvailabilities(this.authenticationService.getUsername(), this.currentLine.id).subscribe(
-      (res) => {
-        this.availabilities = res;
-        this.currentAvailability = 0;
-        this.currentDate = undefined;
-        this.currentDate = new Date(this.availabilities[this.currentAvailability].ride.date).toLocaleDateString("en-GB", {
-          weekday: 'short',
-          year: 'numeric',
-          month: 'long',
-          day: 'numeric'
-        })
+  get selectedRide() {
+    return this.selectedRideIndex == -1 ? null : this.rides[this.selectedRideIndex];
+  }
 
-        this.prevEnabled = true;
-        this.nextEnabled = true;
-        if (this.currentAvailability == 0 || (this.currentAvailability == 1 && this.availabilities[0].date == this.availabilities[1].date)) {
-          this.prevEnabled = false;
-        }
-        if (this.currentAvailability == this.availabilities.length-1 ||
-            (this.currentAvailability == this.availabilities.length-2 &&
-            this.availabilities[this.availabilities.length-1].date == this.availabilities[this.availabilities.length-2].date)) {
-          this.nextEnabled = false;
+  selectLine(line) {
+    this.selectedLine = line;
+    this.loadLine();
+  }
+
+  loadLine() {
+    this.stops = null;
+    this.pupils = null;
+    this.selectedRideIndex = -1;
+    this.rides = null;
+
+    this.attendanceService.getStops(this.selectedLine.id).subscribe(
+      (res) => {
+        this.stops = res;
+      },
+      (error) => {
+        this.handleError(error)
+      }
+    );
+
+    this.attendanceService.getPupils(this.selectedLine.id).subscribe(
+      (res) => {
+        this.pupils = res;
+      },
+      (error) => {
+        this.handleError(error)
+      }
+    );
+
+    this.attendanceService.getRides(this.authenticationService.getUsername(), this.selectedLine.id).subscribe(
+      (res) => {
+        this.rides = res;
+
+        // Pick the closest ride to current date
+        this.selectedRideIndex = this.rides.length-2;
+
+        this.loadRideData();
+      },
+      (error) => {
+        this.handleError(error)
+      }
+    );
+  }
+
+  loadRideData() {
+    console.log('Ciao');
+    this.attendanceService.getRideData(this.selectedRide.id).subscribe(
+      (res) => {
+        this.rideData = res;
+        // Compute missing pupils
+        this.missingPupils = [];
+        for (let pupil of this.pupils) {
+          if (!this.findPupil(pupil)) {
+            this.missingPupils.push(pupil);
+          }
         }
       },
       (error) => {
         this.handleError(error);
       }
-    )
+    );
   }
 
-  loadCurrentRide() {
-    this.reservations$ = this.attendanceService.getReservations(this.currentLine, this.currentDate)
-      .pipe(
-        catchError((error) => {
-          this.handleError(error);
-          throw error; // Propagate error
-        })
-      )
-  }
-
-  onPupilClick(pupil, direction: string) {
-    if (!pupil.disabled) {
-      pupil.disabled = true;
-      if(pupil.attendanceId >= 0){
-        /*Remove attendance from the service*/
-        this.attendanceService.deleteAttendance(this.currentLine, this.currentDate, pupil.attendanceId)
-          .subscribe(
-            () => {
-              pupil.attendanceId = -1;
-              pupil.disabled = false;
-            },
-            (error) => {
-              this.handleError(error);
-              pupil.disabled = false;
-            }
-          );
-
-      } else {
-        /*Add attendance on the service*/ 
-        this.attendanceService.createAttendance(this.currentLine, this.currentDate, pupil.id, direction)
-          .subscribe(
-            (response: any) => {
-              pupil.attendanceId = response.Id;
-              pupil.disabled = false;
-            },
-            (error) => {
-              this.handleError(error);
-              pupil.disabled = false;
-            }
-          );
+  private findPupil(pupil) {
+    for (let stopData of this.rideData.values()) {
+      for (let data of stopData) {
+        if (data.pupil.id == pupil.id) {
+          return true;
+        }
       }
+    }
+
+    return false;
+  }
+
+  onDataClick(data, i) {
+    if (data.type == 'reservation') {
+      console.log(data.stopId);
+      this.attendanceService.createAttendance(data.pupil.id, this.selectedRide.id, data.stopId).subscribe(
+        (res) => {
+          data.attendanceId = res;
+          data.type = 'both';
+        },
+        (error) => {
+          this.handleError(error);
+        }
+      );
+
+    } else {
+      // Both other types represent an attendance, delete it
+      this.attendanceService.deleteAttendance(data.attendanceId).subscribe(
+        (res) => {
+          if (data.type == 'attendance') {
+            // Just deleted an attendance without reservation, delete data an add pupil to missing
+            this.rideData.get(data.stopId).splice(i, 1);
+            this.missingPupils.push(data.pupil)
+          
+          } else {
+            // Just deleted an attendance with reservation, mark data as simple reservation
+            // and remove attendance id
+            data.type = 'reservation';
+            delete data.attendanceId;
+          }
+        },
+        (error) => {
+          this.handleError(error);
+        }
+      );
+
     }
   }
 
-  selectLine(line) {
-    this.currentLine = line;
-    this.lineData$ = this.attendanceService.getLine(this.currentLine.id);
-    this.loadAvailabilities();
+  onPupilClick(pupil, i) {
+    // For now create the reservation for the first stop
+    let stopId = this.stops.outwardStops[0].id;
+    this.attendanceService.createAttendance(pupil.id, this.selectedRide.id, stopId).subscribe(
+      (res) => {
+        // Add the attendance to rideData
+        let data = {
+          attendanceId: res,
+          type: 'attendance',
+          pupil: pupil,
+          stopId: stopId
+        };
+        if (!this.rideData.has(stopId)) {
+          this.rideData.set(stopId, []);
+        }
+        this.rideData.get(stopId).push(data);
+        
+        // Remove the pupil from missing ones
+        this.missingPupils.splice(i, 1);
+      },
+      (error) => {
+        this.handleError(error);
+      }
+    );
+  }
+
+  canNextRide() {
+    if (this.selectedRideIndex == -1) {
+      return false;
+    }
+
+    if (this.selectedRideIndex == this.rides.length-1 ||
+        (this.selectedRideIndex == this.rides.length-2 &&
+         this.rides[this.rides.length-1].date == this.rides[this.rides.length-2].date)) {
+      return false;
+    }
+
+    return true;
   }
 
   nextRide() {
-    if (this.availabilities[this.currentAvailability+1].date == this.availabilities[this.currentAvailability].date) {
-      this.currentAvailability += 2;
+    if (this.rides[this.selectedRideIndex+1].date == this.selectedRide.date) {
+      this.selectedRideIndex += 2;
     } else {
-      this.currentAvailability++;
+      this.selectedRideIndex++;
+    }
+ 
+    this.loadRideData();
+  }
+
+  canPrevRide() {
+    if (this.selectedRideIndex == -1) {
+      return false;
+    }
+    
+    if (this.selectedRideIndex == 0 ||
+        (this.selectedRideIndex == 1 && this.rides[0].date == this.rides[1].date)) {
+      return false;
     }
 
-    this.currentDate = new Date(this.availabilities[this.currentAvailability].ride.date).toLocaleDateString("en-GB", {
-      weekday: 'short',
-      year: 'numeric',
-      month: 'long',
-      day: 'numeric'
-    })
- 
-    // this.loadCurrentRide();
-  
-    this.prevEnabled = true;
-    if (this.currentAvailability == this.availabilities.length-1 ||
-        (this.currentAvailability == this.availabilities.length-2 &&
-        this.availabilities[this.availabilities.length-1].date == this.availabilities[this.availabilities.length-2].date)) {
-      this.nextEnabled = false;
-    }
+    return true;
   }
 
   prevRide() {
-    if (this.availabilities[this.currentAvailability-1].date == this.availabilities[this.currentAvailability].date) {
-      this.currentAvailability -= 2;
+    if (this.rides[this.selectedRideIndex-1].date == this.selectedRide.date) {
+      this.selectedRideIndex -= 2;
     } else {
-      this.currentAvailability--;
+      this.selectedRideIndex--;
     }
 
-    this.currentDate = new Date(this.availabilities[this.currentAvailability].ride.date).toLocaleDateString("en-GB", {
-      weekday: 'short',
-      year: 'numeric',
-      month: 'long',
-      day: 'numeric'
-    })
- 
-    // this.loadCurrentRide();
-    
-    this.nextEnabled = true;
-    if (this.currentAvailability == 0 || (this.currentAvailability == 1 && this.availabilities[0].date == this.availabilities[1].date)) {
-      this.prevEnabled = false;
-    }
+    this.loadRideData();
   }
-
-
 
   private handleError(error: HttpErrorResponse) {
     if (!(error.error instanceof ErrorEvent) && error.status == 401) {
-      /* Not authenticated or auth expired */
+      // Not authenticated or auth expired
       this.router.navigateByUrl('/login');
     
     } else {
-      /* All other errors*/
+      // All other errors
       console.error("Error contacting server");
       this._snackBar.open("Error in the communication with the server!", "", { panelClass: 'error-snackbar', duration: 5000 });
     }
