fc676a975d87606156bfedf92c60601c5ca4e182
Andrea Pavano <andpav@hotmail.it>
2019-10-26 15:44:24 +0200

    .

---
+++ pedibus-frontend/src/app/line.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { environment } from '../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LineService {
+
+  constructor(private http: HttpClient) { }
+
+  getLines(){
+    return this.http.get(environment.apiUrl+'/lines');
+  }
+
+  getStops(lineId: number) {
+    return this.http.get(environment.apiUrl+'/lines/'+lineId);
+  }
+
+  getPupils(lineId: number) {
+    return this.http.get(environment.apiUrl+'/lines/'+lineId+'/pupils');
+  }
+}
