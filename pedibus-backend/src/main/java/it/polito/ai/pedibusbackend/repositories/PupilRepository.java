ba3be6290fb47bca632a8164b1d83245fe8eeb59
unknown <s255183@studenti.polito.it>
2019-12-10 11:58:32 +0100

    second merging push

--- pedibus-backend/src/main/java/it/polito/ai/pedibusbackend/repositories/PupilRepository.java
+++ pedibus-backend/src/main/java/it/polito/ai/pedibusbackend/repositories/PupilRepository.java
@@ -1,9 +1,13 @@
 package it.polito.ai.pedibusbackend.repositories;
 
 import it.polito.ai.pedibusbackend.entities.Pupil;
-import org.springframework.data.repository.CrudRepository;
+import it.polito.ai.pedibusbackend.entities.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.Optional;
 
-public interface PupilRepository extends CrudRepository<Pupil, Long> {
+public interface PupilRepository extends PagingAndSortingRepository<Pupil, Long> {
+    Page<Pupil> findByUser(User user, Pageable pageable);
 }
