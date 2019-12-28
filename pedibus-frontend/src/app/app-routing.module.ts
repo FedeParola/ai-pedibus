525065296e77bb345c3a5ac950627b68bfec18ca
unknown <s255183@studenti.polito.it>
2019-12-28 09:44:07 +0100

    delete register component, data.ts and afterPropertiesSet in reservation service

--- pedibus-frontend/src/app/app-routing.module.ts
+++ pedibus-frontend/src/app/app-routing.module.ts
@@ -5,7 +5,6 @@ import { AttendanceComponent } from './attendance/attendance.component';
 import { AvailabilityComponent } from './availability/availability.component';
 import { ReservationComponent } from './reservation/reservation.component';
 import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
 import { UsersComponent } from './users/users.component';
 import { PupilsComponent } from './pupils/pupils.component';
 import { RidesComponent } from './rides/rides.component';
@@ -13,7 +12,6 @@ import { NotificationComponent } from './notification/notification.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
   { path: 'attendance', component: AttendanceComponent },
   { path: 'users', component: UsersComponent },
   { path: 'availability', component: AvailabilityComponent },
